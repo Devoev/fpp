46d7452683e096660d719bf521e508ac5a9d995b
Devin <devinbalian@gmail.com>
2024-02-27 17:10:29 +0100

    add code for TriangularMesh2D::parse

--- fem/mesh/TriangularMesh2D.cpp
+++ fem/mesh/TriangularMesh2D.cpp
@@ -1,9 +1,17 @@
 
 #include "TriangularMesh2D.h"
+#include <fstream>
 
 using namespace fem::mesh;
 
 TriangularMesh2D TriangularMesh2D::parse(const std::string &msh_path) {
+    std::ifstream file(msh_path);
+    std::string line;
+
+    while (std::getline(file, line)) {
+        // TODO: Parse line and find $Nodes section
+    }
+
     return TriangularMesh2D {}; // TODO: Not yet implemented
 }
 
