b404e47021422b39483e4091325bf22eb4c8f04f
Devin <devinbalian@gmail.com>
2024-02-29 01:08:48 +0100

    add assemble function to nodal_mat.h

--- fem/matrix/nodal_mat.h
+++ fem/matrix/nodal_mat.h
@@ -4,6 +4,39 @@
 namespace fem::mat::nodal {
 
     /**
+     * Assembles a global nodal matrix. Loops over the mesh's triangular elements in parallel.
+     * @param msh 2D triangular mesh object.
+     * @param build_local Builder function for local matrix.
+     * @return Sparse assembled matrix.
+     */
+    Eigen::MatrixXd assemble(
+            const fem::mesh::TriangularMesh2D& msh,
+            const std::function<Eigen::Matrix3d(const Eigen::Matrix<double, 3, 2>&)>& build_local) {
+
+        Eigen::SparseMatrix<double> A(msh.N(), msh.N());
+
+        // Iteration over triangles
+        #pragma omp parallel for
+        for (int t = 0; t < msh.T(); ++t) {
+            auto nodes = msh.elems_to_nodes.row(t);
+            auto coords = msh.nodes(nodes, Eigen::all);
+            Eigen::Matrix3d M_t = build_local(coords);
+
+            // Iteration over local matrix entries
+            for (int i = 0; i < 3; ++i) {
+                Eigen::Index ni = nodes[i];
+                for (int j = 0; j < 3; ++j) {
+                    Eigen::Index nj = nodes[j];
+                    #pragma omp critical
+                    { A.coeffRef(ni, nj) += M_t(i, j); };
+                }
+            }
+        }
+
+        return A;
+    }
+
+    /**
      * Creates the local mass matrix for nodal basis functions.
      * @param nodes Node coordinates.
      */
@@ -21,28 +54,16 @@ namespace fem::mat::nodal {
      * @param msh 2D triangular mesh object.
      */
     Eigen::MatrixXd mass(const fem::mesh::TriangularMesh2D& msh) {
-        Eigen::Index n = msh.N();
-        Eigen::SparseMatrix<double> M(n,n);
+        return assemble(msh, mass_local);
+    }
 
-        // Iteration over triangles
-        #pragma omp parallel for
-        for (int t = 0; t < msh.T(); ++t) {
-            auto nodes = msh.elems_to_nodes.row(t);
-            auto coords = msh.nodes(nodes, Eigen::all);
-            auto M_t = mass_local(coords);
-
-            // Iteration over local matrix entries
-            for (int i = 0; i < 3; ++i) {
-                Eigen::Index ni = nodes[i];
-                for (int j = 0; j < 3; ++j) {
-                    Eigen::Index nj = nodes[j];
-                    #pragma omp critical
-                    { M.coeffRef(ni, nj) += M_t(i,j); };
-                }
-            }
-        }
-
-        return M;
+    /**
+     * Creates the local stiffness matrix for nodal basis functions.
+     * @param nodes Node coordinates.
+     */
+    Eigen::Matrix3d stiffness_local(const Eigen::Matrix<double, 3, 2>& nodes) {
+        // TODO: not yet implemented
+        return Eigen::Matrix3d {};
     }
 
     /**
@@ -50,9 +71,6 @@ namespace fem::mat::nodal {
      * @param msh 2D triangular mesh object.
      */
     Eigen::MatrixXd stiffness(const fem::mesh::TriangularMesh2D& msh) {
-        Eigen::Index n = msh.N();
-        Eigen::Index m = 9 * msh.T();
-        Eigen::SparseMatrix<double> K(n, n);
-        return K;
+        return assemble(msh, stiffness_local);
     }
 }
